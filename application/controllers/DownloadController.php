cfc8a054494025ba4adda87996031362f8fb07a6
Kévin DUBUC <kevindubuc62@gmail.com>
2014-07-30 14:07:04 +0200

    Ajout de la visualisation de fichiers

--- application/controllers/DownloadController.php
+++ application/controllers/DownloadController.php
@@ -3,35 +3,63 @@
 class DownloadController extends Zend_Controller_Action
 {
 
-  public function indexAction()
-  {
+    public function indexAction()
+    {
+        $filesystem = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('filesystem');
 
-    $filesystem = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('filesystem');
+        if (!array_key_exists('file', $_GET)) {
+            header("HTTP/1.0 500 Internal Server Error");
+            exit;
+        }
 
-    if (!array_key_exists('file', $_GET)) {
-      header("HTTP/1.0 500 Internal Server Error");
+        $file = $this->view->escape($_GET['file']);
+
+        if (!$filesystem->has($file)) {
+            header("HTTP/1.0 404 Not Found");
+            exit;
+        }
+
+        $metadata = $filesystem->getMetadata($file);
+
+        header("Pragma: public");
+        header("Expires: -1");
+        header("Cache-Control: public, must-revalidate, post-check=0, pre-check=0");
+        header("Content-Disposition: attachment; filename=\"" . $metadata["basename"] . "\"");
+        header("Content-Type: application/octet-stream");
+
+        echo $filesystem->read($file);
+
         exit;
     }
 
-    $file = $this->view->escape($_GET['file']);
+    public function viewAction()
+    {
+        $filesystem = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('filesystem');
 
-    if (!$filesystem->has($file)) {
-      header("HTTP/1.0 404 Not Found");
-      exit;
+        if (!array_key_exists('file', $_GET)) {
+            header("HTTP/1.0 500 Internal Server Error");
+            exit;
+        }
+
+        $file = $this->view->escape($_GET['file']);
+
+        if (!$filesystem->has($file)) {
+            header("HTTP/1.0 404 Not Found");
+            exit;
+        }
+
+        $mime = $filesystem->getMimetype($file);
+        $metadata = $filesystem->getMetadata($file);
+
+        header("Pragma: public");
+        header("Expires: -1");
+        header("Cache-Control: public, must-revalidate, post-check=0, pre-check=0");
+        header("Content-Disposition: inline; filename=\"" . $metadata["basename"] . "\"");
+        header("Content-Type: $mime");
+
+        echo $filesystem->read($file);
+
+        exit;
     }
-
-    $metadata = $filesystem->getMetadata($file);
-
-    header("Pragma: public");
-    header("Expires: -1");
-    header("Cache-Control: public, must-revalidate, post-check=0, pre-check=0");
-    header("Content-Disposition: attachment; filename=\"" . $metadata["basename"] . "\"");
-    header("Content-Type: application/octet-stream");
-
-    echo $filesystem->read($file);
-
-    exit;
-
-  }
 
 }
