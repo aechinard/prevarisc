77ac4957df2749d0808c81f6b9fbacd3a8b0aeeb
Kévin DUBUC <kevindubuc62@gmail.com>
2014-08-01 15:18:11 +0200

    On change le contexte des erreurs

--- application/controllers/NewsController.php
+++ application/controllers/NewsController.php
@@ -14,7 +14,7 @@ class NewsController extends Zend_Controller_Action
                 $service_feed->addMessage($this->_request->getParam('type'), $this->_request->getParam('text'), Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR'], $this->_request->getParam('conf') );
                 $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message ajouté !','message' => 'Le message a bien été ajouté.'));
             } catch (Exception $e) {
-                $this->_helper->flashMessenger(array('context' => 'error','title' => 'Erreur !','message' => 'Erreur lors de l\'ajout du message : ' . $e->getMessage()));
+                $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur !','message' => 'Erreur lors de l\'ajout du message : ' . $e->getMessage()));
             }
 
             $this->_helper->redirector->gotoRoute(array(), 'home', true);
@@ -28,7 +28,7 @@ class NewsController extends Zend_Controller_Action
             $service_feed->deleteMessage($this->_request->getParam('id'));
             $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message supprimé !','message' => 'Le message a bien été supprimé.'));
         } catch (Exception $e) {
-            $this->_helper->flashMessenger(array('context' => 'error','title' => 'Erreur !','message' => 'Erreur lors de la suppression du message : ' . $e->getMessage()));
+            $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur !','message' => 'Erreur lors de la suppression du message : ' . $e->getMessage()));
         }
 
         $this->_helper->redirector->gotoRoute(array(), 'home', true);
