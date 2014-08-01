0a262739bc36e6c75d963a0fa3f7b27ba3911fa7
Kévin DUBUC <kevindubuc62@gmail.com>
2014-08-01 15:06:47 +0200

    fullCalendar en français !

--- www/assets/js/plugins.js
+++ www/assets/js/plugins.js
@@ -15,4 +15,24 @@
     return hide.apply(this, arguments);
   };
 
+  /*
+  # MonkeyPatch pour traduire fullCalendar en français
+  */
+  var fullCalendar;
+  fullCalendar = $.fn.fullCalendar;
+  $.fn.fullCalendar = function(options) {
+
+    options.buttonText = options.buttonText || {month: 'Mois', day: 'Jour', week: 'Semaine', today: 'Aujourd\'hui'};
+    options.header = options.header || {left: 'prev,next today', center: 'title', right: 'month,agendaWeek,agendaDay'};
+    options.titleFormat = options.titleFormat || {month: 'MMMM yyyy', week: "dd[ MMMM][ yyyy]{ '&#8212;' dd MMMM yyyy}", day: 'dddd dd MMMM yyyy'};
+    options.firstDay = options.firstDay || 1;
+    options.monthNames = options.monthNames || ['Janvier','F\u00e9vrier','Mars','Avril','Mai','Juin','Juillet','Ao\u00fbt','Septembre','Octobre','Novembre','D\u00e9cembre'];
+    options.monthAbbrevs = options.monthAbbrevs || ['Jan','Fev','Mar','Avr','Mai','Juin','Juil','Aout','Sep','Oct','Nov','Dec'];
+    options.dayNames = options.dayNames || ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'];
+    options.dayNamesShort = options.dayNamesShort || ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam','Dim'];
+    options.weekMode = options.weekMode || 'liquid';
+
+    return fullCalendar.apply(this, arguments);
+  };
+
 })(jQuery);
