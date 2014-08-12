43f20ab99f9497e9ca9a69b27988d0bbbdf0aea3
Kévin DUBUC <kevindubuc62@gmail.com>
2014-08-12 11:41:33 +0200

    Proto d'une nouvelle gestion des erreurs

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -4,50 +4,37 @@ class ErrorController extends Zend_Controller_Action
 {
     public function errorAction()
     {
-        $this->_helper->layout->setLayout('error');
-
+        $message = "";
         $errors = $this->getParam('error_handler');
 
+        // Gestion du message de l'erreur
         if (!$errors || !$errors instanceof ArrayObject) {
-            $this->view->message = 'Vous avez atteint la page d\'erreur';
-            return;
+            $message = "L'application s'est arrêtée brutalement.";
+        } else {
+            switch ($errors->type) {
+                case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
+                case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
+                case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+                    $message = "Page introuvable.";
+                    break;
+            }
         }
 
-        // On envoie le bon code erreur en fonction du type
-        switch ($errors->type) {
-            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
-            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
-            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-                $this->getResponse()->setHttpResponseCode(404);
-                $this->view->message = 'Page introuvable';
-                break;
-
-            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_OTHER:
-                if ($errors->exception->getCode() == 401) {
-                    $this->getResponse()->setHttpResponseCode(401);
-                    $this->render('not-allowed');
-                }
-                break;
-
-            default:
-                $this->getResponse()->setHttpResponseCode(500);
-                $this->view->message = 'L\'application a levée une erreur';
-                break;
+        // Si l'affichage des exceptions est activé, on ajoute le message d'erreur
+        if (getenv('PREVARISC_DEBUG_ENABLED')) {
+            $message .= ' (' . $errors->exception->getMessage() . ')';
         }
 
         // On log l'erreur
-        $logger = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('logger');
-        $logger->addError('Erreur ' . $this->view->message, array(
+        $this->getInvokeArg('bootstrap')->getResource('logger')->addError('Erreur ' . $message, array(
             'params' => $errors->request->getParams(),
             'exception' => $errors->exception
         ));
 
-        // Si l'affichage des exceptions est activé, on envoie un message
-        if (getenv('PREVARISC_DEBUG_ENABLED')) {
-            $this->view->exception = $errors->exception;
-        }
+        // On ajoute le message d'erreur à la stack des messages
+        $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur','message' => $message));
 
-        // On envoie la requête de l'erreur sur la vue
-        $this->view->request = $errors->request;
+        // On retourne sur la home
+        $this->_helper->redirector->gotoRoute(array(), 'home', true);
     }
 }
