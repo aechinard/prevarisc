a599865dd63f1bd87e52f427bcb684556a5db1dc
Kévin DUBUC <kevindubuc62@gmail.com>
2014-09-30 09:51:34 +0200

    Ajout méthode get ds service Gpt

--- application/services/GroupementCommunes.php
+++ application/services/GroupementCommunes.php
@@ -18,4 +18,16 @@ class Service_GroupementCommunes extends Service_Abstract
 
         return $model_groupement->get()->toArray();
     }
+
+    /**
+     * Récupération d'un groupement de commune
+     *
+     * @param int id Optionnel
+     * @return array
+     */
+    public function get($id)
+    {
+        $model_groupement = new Model_DbTable_Groupement;
+        return $model_groupement->find($id)->current()->toArray();
+    }
 }
